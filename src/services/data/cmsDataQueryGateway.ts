54381cd5dab3b21c02c3d5fcc35b8e3b2124a70c
Steven Cao <lucct9389@gmail.com>
2025-03-26 10:47:30 +0700

    HEAD-114: add default footer in sanity

--- src/services/data/cmsDataQueryGateway.ts
+++ src/services/data/cmsDataQueryGateway.ts
@@ -91,7 +91,9 @@ export async function fetchAPIGatewayWrapper(details: DynamicFileModuleDetails, 
 
         return await fetchAPI(details, endpoint, headers, pageAndSingleComponentDetails);
     } else if(cmsVariant === 'sanity') {
-      const endpoint = `${CmsVariants.variants[cmsVariant].deliveryApiDomain}`;
+      const projectId = process.env.SANITY_STUDIO_PROJECT_ID || "5he8nsc5";
+      const defaultEndpoint = `https://${projectId}.api.sanity.io/v2023-08-01/graphql/production/default`;
+      const endpoint = CmsVariants.variants[cmsVariant].deliveryApiDomain || defaultEndpoint;
       
       log.trace(`${logPrefix()}[${pageAndSingleComponentDetails?.component.identifier}] fetchAPIGatewayWrapper - endpoint ::: ${endpoint}`);
 
